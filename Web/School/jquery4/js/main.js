aca47f3f252fc2f41696749d6027d61cc567d9fa
Kyle John <njk230@gmail.com>
2017-05-29 10:15:41 +0900

    Update jquery4

---
+++ Web/School/jquery4/js/main.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $(window).scroll(function () {
+        var h = $(window).scrollTop();
+
+        if (h >= 0 && h < 1000) {
+            $(".main_bg").removeClass("on");
+            $("#area1").addClass("on");
+        } else if (1000 <= h && h < 2000) {
+            $(".main_bg").removeClass("on");
+            $("#area2").addClass("on");
+        } else if (2000 <= h && h < 3000) {
+            $(".main_bg").removeClass("on");
+            $("#area3").addClass("on");
+        } else {
+            $(".main_bg").removeClass("on");
+            $("#area4").addClass("on");
+        }
+    });
+
+    $(window).trigger("scroll");
+});
